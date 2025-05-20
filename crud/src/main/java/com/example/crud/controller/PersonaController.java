34d4d44533b379859b124ac93f1f9f68ff16f37a
jorgeStevenR <rodrigesj500@gmail.com>
2025-05-20 13:30:00 -0500

    Put de personas

--- crud/src/main/java/com/example/crud/controller/PersonaController.java
+++ crud/src/main/java/com/example/crud/controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +57,24 @@ public class PersonaController {
     public ResponseEntity<Optional<Persona>> ListarPersonasPorId(@PathVariable("id") long id){
         return ResponseEntity.ok(personaServices.findById(id));
         
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Persona> actualizarPersona(@PathVariable Long id, @RequestBody Persona personaActualizada){
+        Optional<Persona> personaOptional = personaServices.findById(id);
+
+        if (personaOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        Persona personaExistente = personaOptional.get();
+
+        personaExistente.setNombre(personaActualizada.getNombre());
+        personaExistente.setApellido(personaActualizada.getApellido());
+        personaExistente.setCorreo(personaActualizada.getCorreo());
+
+        Persona personaGuardada = personaServices.createPersona(personaExistente);
+        return ResponseEntity.ok(personaGuardada);
     }
 
 }
